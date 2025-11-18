e974db9ab2bc1b480fd8a261d4027b7a1427ca95
nathanlegit <154447733+nathanlegit@users.noreply.github.com>
2025-11-18 13:06:27 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,258 @@
+from tensorflow.keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
+import numpy as np
+
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+from streamlit_option_menu import option_menu
+import time
+
+import os
+import openai
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+# Teachable Machine model.
+# Input files: model export, labels export, uploaded image.
+def imageModel(image_model, labels_file, image_file):
+    # Disable scientific notation for clarity
+    np.set_printoptions(suppress=True)
+
+    # Load the model
+    model = load_model(image_model, compile=False)
+
+    # Load the labels
+    class_names = open(labels_file, "r").readlines()
+
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    # Replace this with the path to your image
+    image = Image.open(image_file).convert("RGB")
+
+    # resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+    # turn the image into a numpy array
+    image_array = np.asarray(image)
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # Predicts the model
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    # NOT Print prediction and confidence score
+    print("Confidence Score:", confidence_score)
+    #confidence_percentage = str(confidence_score*100) + "%" + " confidence"
+    return (class_name[2:-1])
+
+@st.cache_data
+def openai_completion(prompt):
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=prompt,
+      max_tokens=500,
+      temperature=0.5
+    )
+    output = response['choices'][0]['text']
+    return output
+
+# ⭐ Streamlit UI ⭐
+st.title("Military Environment Analysis Algorithm (M.E.A.A G009)")
+st.header("The Future of Military Operation Planning")
+
+with st.sidebar:
+
+    Logo = Image.open('MEAA.png')
+    st.image(Logo)   
+    st.write("Welcome to M.E.A.A, a cutting-edge algorithm designed to analyse battleground terrains and environments in a speedy and holistic manner")
+    st.divider()
+    st.subheader("How to use this app:")
+    st.write("Home: Read through this section for a brief introduction and explanation on the workings of the app.")
+    st.write("Upload: For the main function of the app; uploading a terrain image and getting a full and detailed description on battle strategies, equipment, and optimal weapons used")
+    st.write("Image-capture: For urgent uses, to upload a quick snap of the battleground from a device to get a analysis from the app")
+    st.write("Health and safety: To provide a comprehensive summary of the health and sleep schedules optimal for battle for soldiers in the army")
+
+
+# 5. Add on_change callback
+def on_change(key):
+    selection = st.session_state[key]
+    
+selected5 = option_menu(None, ["Home", "Upload", "Live-Image Capture", 'Health Scheduling'],
+                        icons=['house', 'cloud-upload', "list-task", 'gear'],
+                        on_change=on_change, key='menu_5', orientation="horizontal")
+
+if selected5 == "Home":
+    home_image1 = Image.open('home_image1.jfif')
+    st.image(home_image1, caption='Cutting-Edge Defense Technology')
+    add_selectbox = st.selectbox(
+    "Choose what to read about",
+    ("Introduction", "How it works", "Further expansions")
+)
+    
+    if add_selectbox == "Introduction":
+        st.write("Welcome to the future of military operations. A revolutionary algorithm designed with cutting edge Artificial intelligence technology, coupled with accurate and detailed image recognition capabilities, the Military Environment Analysis Alogrithm (M.E.A.A) is one of a kind.")
+        st.write("Being able to detect different surroundings and environments in a blink of an eye from a simple image uploaded onto the platform, mlitary operational personnel are able to utilise these inputs in the formulation or reevaluation of their battle plans and strategies. Information on equipment and weaponry used can also be useful for logistical preperation and gaining hardware advantage on opponents in the heat of the battle.")
+        st.divider()
+        st.markdown("All rights reserved: Group 9")
+
+    if add_selectbox == "How it works":
+        st.subheader("Terrain Generation")
+        st.write("Utlitising the open code readily avaliable from Teachable machines, we have developed a 3 tiered Machine learning model taking into account 3 environmental parameters: Terrain, Landscape and Presence of snow. By using data in the form of pictures found on open source websites such as pexels, we trained the learning algorthm to be able to classify any image uploaded into catagories for these 3 parameters.We included more than 20 pictures in each data set to increase reliability and accuracy of classification, which allows us to effectively and accurately classify images into catagories which accurately describe the military environment. ")
+        home_image2 = Image.open('Home_image2.png')
+        st.image(home_image2, caption='Teachable Machines')
+        st.divider()
+        st.subheader("Use of AI to generate response")
+        st.write("We then inputted the terrain characteristics into an GenAI model and enetered three different prompts to generate military strategies, equipment required and weaponry that is optimal for that particular military environment. The AI that we used was trained on information found online, however, for the real algorithm, it will be trained on confidential and real military operations in the past for official strategies.")
+        home_image3 = Image.open('home_image3.jfif')
+        st.image(home_image3, caption='Artificial Intelligence')
+        st.divider()
+        st.subheader("App Development")
+        st.write("We used Streamlit to design our user interface and application platform. By lots of trial and error and searching the documentation for Streamlit online, we have developed a user-friendly, interactive app that is comprehensive and easy to use.")
+        home_image4 = Image.open('home_image4.png')
+        st.image(home_image4, caption='Steamlit documentation')
+        st.divider()
+        st.markdown("All rights reserved: Group 9")
+
+    if add_selectbox == "Further expansions":
+        st.write("Here are some further improvements that we hope to make to M.A.E.E")
+        st.divider()
+        tab1, tab2, tab3 = st.tabs(["Factors", "Data", "Functionality"])
+
+        with tab1:
+            st.header("More factors")
+            st.write("Have more factors considered in the generation of an optimal military strategy. These factors can include temperature, weather, and wind")
+            home_image5 = Image.open('home_image5.jpg')
+            st.image(home_image5, caption='Weather')
+
+
+        with tab2:
+            st.header("Real-Life Data")
+            st.write("We could utliise more real time data for the temperature and the wind from weather sites so as to provide more up to date information and military advice")
+            home_image6 = Image.open('home_image6.jfif')
+            st.image(home_image6, caption='Weather')
+
+        with tab3:
+            st.header("Functionality")
+            st.write("Based on the advice given, more functions could be added, such as a training function which gives the military a training programme based the type of warfare and offense strategy advised")
+            home_image7 = Image.open('home_image7.png')
+            st.image(home_image7, caption='Weather')
+
+# ⭐ End of Streamlit UI ⭐
+
+if selected5 == "Upload":
+    st.subheader("Instructions")
+    st.write("Make sure your photo captures a full image of the terrain, including important physical features such as mountains, waterbodies, buildings and snow")
+    home_image8 = Image.open('Test 3 Snow.jpg')
+    st.image(home_image8, caption='Sample of a Winter, Plain terrain')
+    st.divider()
+    st.subheader("Photo Upload")
+    # File uploader/saver 
+    # Runs all 3 models, outputs an array. 
+    uploaded_file = st.file_uploader("Please upload a photo of the terrain")
+    loading = st.empty()
+    if uploaded_file is not None:
+        im = Image.open(uploaded_file)
+        try: 
+            with loading.container():
+                with st.spinner("Loading...💫"): #doesn't work
+                    loading_indicator = st.write("loading... please wait") # Shows "loading" text
+
+            im.save("sample.png") 
+            description = []
+            description += [imageModel("terrain_model.h5", "terrain_labels.txt", "sample.png")]
+            description += [imageModel("landscape_model.h5", "landscape_labels.txt", "sample.png")]
+            description += [imageModel("snownosnow_model.h5", "snownosnow_labels.txt", "sample.png")]
+            
+            description = ", ".join(description)
+            loading.empty() # Removes "loading" text after done loading
+
+            prompt_text_1 = f"You are an assistant helping the military of a country plan for a battle, given a description of the environment. Give us some battlefield strategies for an environment with these characteristics: {description}." 
+            prompt_text_2 = f"You are an assistant helping the military of a country plan for a battle, given a description of the environment. Give me some military equipment that soldiers will need for an environment with these characteristics: {description}."
+            prompt_text_3 = f"You are an assistant helping the military of a country plan for a battle, given a description of the environment. Give me some weaponry that the military will need for an environment with these characteristics: {description}."
+            openai_answer_1 = openai_completion(prompt_text_1)
+            openai_answer_2 = openai_completion(prompt_text_2)
+            openai_answer_3 = openai_completion(prompt_text_3)
+            
+            st.write(f"Terrain type: {description}")
+            tab4, tab5, tab6 = st.tabs(["Military Strategy", "Equipment", "Weaponry"])
+
+            with tab4:
+                st.header("Military Strategy")
+                st.write(openai_answer_1)
+
+            with tab5:
+                st.header("Equipment Required")
+                st.write(openai_answer_2)
+                
+            with tab6:
+                st.header("Optimal Weaponry")
+                st.write(openai_answer_3)
+
+        except IOError:
+            print("Error! :(") 
+    
+    st.divider()
+    st.markdown("All rights reserved: Group 9")
+
+if selected5 == "Live-Image Capture":
+    picture = st.camera_input("Take a clear picture of you surroundings")
+
+    loading = st.empty()
+    if picture is not None:
+        im = Image.open(picture)
+        try: 
+            with loading.container():
+                with st.spinner("Loading...💫"): #doesn't work
+                    loading_indicator = st.write("loading... please wait") # Shows "loading" text
+
+            im.save("sample.png") 
+            description = []
+            description += [imageModel("terrain_model.h5", "terrain_labels.txt", "sample.png")]
+            description += [imageModel("landscape_model.h5", "landscape_labels.txt", "sample.png")]
+            description += [imageModel("snownosnow_model.h5", "snownosnow_labels.txt", "sample.png")]
+            
+            description = ", ".join(description)
+            loading.empty() # Removes "loading" text after done loading
+
+            prompt_text_1 = f"You are an assistant helping the military of a country plan for a battle, given a description of the environment. Give us some battlefield strategies for an environment with these characteristics: {description}." 
+            prompt_text_2 = f"You are an assistant helping the military of a country plan for a battle, given a description of the environment. Give me some military equipment that soldiers will need for an environment with these characteristics: {description}."
+            prompt_text_3 = f"You are an assistant helping the military of a country plan for a battle, given a description of the environment. Give me some weaponry that the military will need for an environment with these characteristics: {description}."
+            openai_answer_1 = openai_completion(prompt_text_1)
+            openai_answer_2 = openai_completion(prompt_text_2)
+            openai_answer_3 = openai_completion(prompt_text_3)
+            
+            st.write(f"Terrain type: {description}")
+            tab4, tab5, tab6 = st.tabs(["Military Strategy", "Equipment", "Weaponry"])
+
+            with tab4:
+                st.header("Military Strategy")
+                st.write(openai_answer_1)
+
+            with tab5:
+                st.header("Equipment Required")
+                st.write(openai_answer_2)
+                
+            with tab6:
+                st.header("Optimal Weaponry")
+                st.write(openai_answer_3)
+
+        except IOError:
+            print("Error! :(") 
+
+
+
+
+
+
